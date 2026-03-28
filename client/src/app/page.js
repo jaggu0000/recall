11364ec15944bec5785ed502ce62094e2c97e1b4
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2026-03-29 03:13:19 +0530

    geo location

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,12 @@
 import FaceRecognition from "@/components/FaceRecongnition";
 
+import LocationTracker from "@/components/GeoLocation";
+
 export default function Home() {
   return (
     <>
       <FaceRecognition />
+      <LocationTracker />
     </>
   );
 }
